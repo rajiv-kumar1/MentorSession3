35f9c1424bb0be1051ab653354c0d5d3f4d1f186
hacker <120033369+rajiv-kumar1@users.noreply.github.com>
2023-01-26 17:51:14 -1200

    Create Arraylistdemo.java

---
+++ DSA/Test/Arraylistdemo.java
@@ -0,0 +1,15 @@
+package com.gl.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		
+		System.out.println(list);
+
+	}
+
+}
